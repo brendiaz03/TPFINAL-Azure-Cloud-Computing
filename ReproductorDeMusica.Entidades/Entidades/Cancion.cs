c6308e1d5c3cd61919b44a90eeed38116c8a1400
AlanAruFlores <alanaruquipa242@gmail.com>
2024-10-13 11:38:57 -0300

    orm

---
+++ ReproductorDeMusica.Entidades/Entidades/Cancion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReproductorDeMusica.Entidades.Entidades;
+
+public partial class Cancion
+{
+    public int Id { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string Artista { get; set; } = null!;
+
+    public string Album { get; set; } = null!;
+
+    public int Creador { get; set; }
+
+    public int Duracion { get; set; }
+
+    public virtual Usuario CreadorNavigation { get; set; } = null!;
+
+    public virtual ICollection<ListaCancione> ListaCanciones { get; set; } = new List<ListaCancione>();
+}
---
+++ ReproductorDeMusica.Entidades/Entidades/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReproductorDeMusica.Entidades.Entidades;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string NombreUsuario { get; set; } = null!;
+
+    public string Contrasenia { get; set; } = null!;
+
+    public int IdPlan { get; set; }
+
+    public virtual ICollection<Cancion> Cancions { get; set; } = new List<Cancion>();
+
+    public virtual Plan IdPlanNavigation { get; set; } = null!;
+
+    public virtual ICollection<ListaReproduccion> ListaReproduccions { get; set; } = new List<ListaReproduccion>();
+}
