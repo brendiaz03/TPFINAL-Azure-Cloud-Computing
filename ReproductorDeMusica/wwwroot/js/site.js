903be6136b364563951431d4cd7c527982bdb525
Lorena <juhal.lorena@gmail.com>
2024-10-19 14:03:31 -0300

    Vista home

--- ReproductorDeMusica/wwwroot/js/site.js
+++ ReproductorDeMusica/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Inicializar comportamiento de carrusel
+const carousels = document.querySelectorAll('.carousel');
 
-// Write your JavaScript code.
+carousels.forEach(carousel => {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    carousel.addEventListener('mousedown', (e) => {
+        isDown = true;
+        startX = e.pageX - carousel.offsetLeft;
+        scrollLeft = carousel.scrollLeft;
+    });
+
+    carousel.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+
+    carousel.addEventListener('mouseup', () => {
+        isDown = false;
+    });
+
+    carousel.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - carousel.offsetLeft;
+        const walk = (x - startX) * 3; // Ajustar la velocidad de desplazamiento
+        carousel.scrollLeft = scrollLeft - walk;
+    });
+});
+
+// Agregar eventos a los botones de autenticación
+document.querySelector('.login-btn').addEventListener('click', () => {
+    alert('Iniciar sesión clickeado');
+});
+
+document.querySelector('.register-btn').addEventListener('click', () => {
+    alert('Registrarse clickeado');
+});
