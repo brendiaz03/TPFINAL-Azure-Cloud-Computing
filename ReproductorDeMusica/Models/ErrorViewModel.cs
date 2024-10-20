72c2122cd3f12bc9fa98e9e4851a01dac9a2788f
Brenda Diaz <brendiaaz03@gmail.com>
2024-10-20 16:57:42 -0300

    Remove ignored files from index

--- ReproductorDeMusica/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ReproductorDeMusica.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
