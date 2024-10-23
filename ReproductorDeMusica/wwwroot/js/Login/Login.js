f7b59d498950479481fa222e65de95d76326de32
Bianca Leites <bleites@circostudio.com>
2024-10-23 14:24:03 -0300

    organización de carpetas y visibilidad de password

---
+++ ReproductorDeMusica/wwwroot/js/Login/Login.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('passwordIcon').addEventListener('click', function () {
+        const passwordInput = document.getElementById('passwordInput');
+        const passwordIcon = document.getElementById('passwordIcon');
+
+        // Alternar tipo de entrada
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            passwordIcon.classList.remove('fa-eye-slash');
+            passwordIcon.classList.add('fa-eye');
+        } else {
+            passwordInput.type = 'password';
+            passwordIcon.classList.remove('fa-eye');
+            passwordIcon.classList.add('fa-eye-slash');
+        }
+    });
+});
