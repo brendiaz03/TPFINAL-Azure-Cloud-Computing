dd83efcb2523e63386ff5e9a10ada4a3e2bd1761
Facundo Varela <111824912+facuvarela00@users.noreply.github.com>
2024-11-03 22:39:53 -0300

    uplan service
    a

---
+++ ReproductorDeMusica.Logica/Interfaces/UsuarioPlanService.cs
@@ -0,0 +1,14 @@
+﻿using ReproductorDeMusica.Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReproductorDeMusica.Logica.Interfaces
+{
+    public interface IUsuarioPlanService
+    {
+        Usuario ObtenerUsuarioConPlan(int usuarioId);
+    }
+}
