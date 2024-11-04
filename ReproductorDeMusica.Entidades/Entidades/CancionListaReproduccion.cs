0232315c85f4c31bd4c663d544b0b6e02b951569
AlanAruFlores <alanaruquipa242@gmail.com>
2024-11-04 18:07:41 -0300

    nuevo scaffold

--- ReproductorDeMusica.Entidades/Entidades/CancionListaReproduccion.cs
+++ ReproductorDeMusica.Entidades/Entidades/CancionListaReproduccion.cs
@@ -11,6 +11,8 @@ public partial class CancionListaReproduccion
 
     public int? IdCancion { get; set; }
 
+    public bool? Eliminado { get; set; }
+
     public virtual Cancion? IdCancionNavigation { get; set; }
 
     public virtual ListaReproduccion? IdListaReproduccionNavigation { get; set; }
